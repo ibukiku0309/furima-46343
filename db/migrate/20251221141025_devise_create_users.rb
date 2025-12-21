c4028c3de54026b2c9c2ca37785f456c25e0a5ca
ibukiku0309 <ibukikikuyama@gmail.com>
2025-12-21 23:42:14 +0900

    テーブル作成

--- db/migrate/20251221141025_devise_create_users.rb
+++ db/migrate/20251221141025_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :lastname,           null: false
+      t.string :firstname,          null: false
+      t.string :lastnamekana,       null: false
+      t.string :firstnamekana,      null: false
+      t.date   :birthday,           null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
