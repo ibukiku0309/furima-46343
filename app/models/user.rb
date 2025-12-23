2552c257189edb63751a275289fd9367425ce7e0
ibukiku0309 <ibukikikuyama@gmail.com>
2025-12-23 21:02:37 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :lastname
+    validates :firstname
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナで入力してください' } do
+    validates :lastnamekana
+    validates :firstnamekana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
