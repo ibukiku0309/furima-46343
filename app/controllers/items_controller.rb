8d5b60e12a51752b103948c9b71ec6e941130d3e
ibukiku0309 <ibukikikuyama@gmail.com>
2026-01-07 23:07:39 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :content, :category_id, :condition_id, :fee_id, :prefecture_id,
+                                 :shippingtime_id, :price).merge(user_id: current_user.id)
+  end
 end
