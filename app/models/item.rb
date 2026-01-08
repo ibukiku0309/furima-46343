998f810fad7e822413910cb94ca8c6d7a2c9ea7b
ibukiku0309 <ibukikikuyama@gmail.com>
2026-01-08 22:53:30 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,19 @@ class Item < ApplicationRecord
   belongs_to :fee
   belongs_to :prefecture
   belongs_to :shippingtime
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shippingtime_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shippingtime_id, numericality: { other_than: 1, message: "can't be blank" }
 end
