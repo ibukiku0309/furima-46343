ec516298e63241747366564049bc219a245be44a
ibukiku0309 <ibukikikuyama@gmail.com>
2026-01-12 16:09:37 +0900

    出品した商品をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
