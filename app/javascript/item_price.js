9c9d9b60badf5dcfa057145557beddadfb5e1b6d
ibukiku0309 <ibukikikuyama@gmail.com>
2026-01-17 00:43:50 +0900

    修正１

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,6 @@ const price = () => {
       const priceInput = document.getElementById("item-price");
       priceInput.addEventListener("input", () => {
          const inputValue = priceInput.value;
-         console.log(inputValue);
          const addTaxDom = document.getElementById("add-tax-price");
          addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
          const profitDom = document.getElementById("profit");
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,6 +65,31 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+      it 'priceが300未満では保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが9_999_999を超えると保存できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it 'priceが全角数字だと保存できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが文字列だと保存できない' do
+        @item.price = 'abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'userが存在しないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
