246c428476570f31ae48162346d01b5b79a2cb49
ibukiku0309 <ibukikikuyama@gmail.com>
2026-02-01 15:34:56 +0900

    途中経過３

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,17 @@
 class RecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @record = Record.new
+    @record = RecordsAddresses.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @record = RecordsAddresses.new
+    if @record.valid?
+      @record.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 end
